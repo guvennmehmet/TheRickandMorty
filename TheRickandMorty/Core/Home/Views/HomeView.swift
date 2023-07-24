e6bc7fb25081a4e2c799d8fabda305d99dae9d2c
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-24 09:35:08 +0300

    Created HomeView.

---
+++ TheRickandMorty/Core/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  TheRickandMorty
+//
+//  Created by Mehmet Güven on 24.07.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
