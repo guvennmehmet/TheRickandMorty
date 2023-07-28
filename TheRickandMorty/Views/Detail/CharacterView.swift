7395477dc98e750e054827c222ba764167c3b3eb
Mehmet Güven <guvennmehmet37@gmail.com>
2023-07-28 16:58:29 +0300

    Updated the CharacterView

--- TheRickandMorty/Views/Detail/CharacterView.swift
+++ TheRickandMorty/Views/Detail/CharacterView.swift
@@ -12,42 +12,47 @@ struct CharacterView: View {
     
     var body: some View {
         ScrollView {
-            AsyncImage(url: URL(string: character.image)) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-            } placeholder: {
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white.opacity(0.7))
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            }
-            .frame(height: 300)
-            .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
-            
-            VStack(spacing: 30) {
-                Text(character.name)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.center)
-                
-                VStack(alignment: .leading, spacing: 30) {
-                    if !character.gender.isEmpty {
-                        Text(character.gender)
+            VStack(spacing: 0) {
+                ZStack(alignment: .top) {
+                    AsyncImage(url: URL(string: character.image)) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 300)
+                    } placeholder: {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100, alignment: .center)
+                            .foregroundColor(.white.opacity(0.7))
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
-                    Text(character.species)
-                    Text(character.status)
-                    Text(character.created)
-                    Text(character.type)
-                    Text(character.location.name)
+                    .frame(height: 300)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
+                .ignoresSafeArea(.container, edges: .top)
+                
+                VStack(spacing: 30) {
+                    Text(character.name)
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    
+                    VStack(alignment: .leading, spacing: 30) {
+                        if !character.gender.isEmpty {
+                            Text(character.gender)
+                        }
+                        Text(character.species)
+                        Text(character.status)
+                        Text(character.created)
+                        Text(character.type)
+                        Text(character.location.name)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
-        .ignoresSafeArea(.container, edges: .top)
     }
 }
 
@@ -56,3 +61,4 @@ struct CharacterView_Previews: PreviewProvider {
         CharacterView(character: Character.all[0])
     }
 }
+
