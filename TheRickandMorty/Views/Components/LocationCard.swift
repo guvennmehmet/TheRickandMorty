d22b1de529c27e8f2ed707b90c79833b4c554ca2
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-01 13:21:44 +0300

    Created the LocationCard

---
+++ TheRickandMorty/Views/Components/LocationCard.swift
@@ -0,0 +1,64 @@
+//
+//  LocationCard.swift
+//  TheRickandMorty
+//
+//  Created by Mehmet Güven on 1.08.2023.
+//
+
+import SwiftUI
+
+struct LocationCard: View {
+    var location: LocationModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            LabelTextView(label: "Name: ", value: location.name)
+            LabelTextView(label: "Type: ", value: location.type)
+            LabelTextView(label: "Dimension: ", value: location.dimension)
+            LabelTextView(label: "Created: ", value: location.created)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(color: Color.gray.opacity(0.7), radius: 4, x: 0, y: 4)
+    }
+}
+
+struct LabelTextView: View {
+    var label: String
+    var value: String
+    
+    var body: some View {
+        HStack {
+            Text(label)
+                .styleLabel()
+            
+            Text(value)
+                .styleValue()
+            
+            Spacer()
+        }
+    }
+}
+
+extension Text {
+    func styleLabel() -> some View {
+        self
+            .font(.headline)
+            .fontWeight(.bold)
+            .foregroundColor(.red)
+    }
+    
+    func styleValue() -> some View {
+        self
+            .font(.subheadline)
+    }
+}
+
+struct LocationCard_Previews: PreviewProvider {
+    static var previews: some View {
+        let location = LocationModel(id: 1, name: "Earth", type: "Planet", dimension: "C-137", residents: [], url: "https://rickandmortyapi.com/api/location/1", created: "2023-08-01T12:00:00Z")
+        
+        LocationCard(location: location)
+    }
+}
