2cc22a9625235b051aff922b9d1d2c4a671b6e0c
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-02 15:15:20 +0300

    Created the EpisodeModel

---
+++ TheRickandMorty/Models/EpisodeModel.swift
@@ -0,0 +1,31 @@
+//
+//  EpisodeModel.swift
+//  TheRickandMorty
+//
+//  Created by Mehmet Güven on 2.08.2023.
+//
+
+import Foundation
+
+struct Episode: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let airDate: String
+    let episodeNumber: String
+    let url: String
+    let created: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case airDate = "air_date"
+        case episodeNumber = "episode_number"
+        case url
+        case created
+    }
+}
+
+struct EpisodeList: Codable {
+    let results: [Episode]
+}
+
