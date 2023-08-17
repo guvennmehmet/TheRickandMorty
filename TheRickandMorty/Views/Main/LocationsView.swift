d92d2d4cd5f3e92bd9daeae797a49b0d9bb8f1f3
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-17 13:11:16 +0300

    Updated the LocationsView(Added the ScrollToTopButton)

--- TheRickandMorty/Views/Main/LocationsView.swift
+++ TheRickandMorty/Views/Main/LocationsView.swift
@@ -9,28 +9,70 @@ import SwiftUI
 
 struct LocationsView: View {
     @ObservedObject var locationViewModel: LocationViewModel
+    @State var scrollViewOffset: CGFloat = 0
+    @State var startOffset: CGFloat = 0
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LocationList(locationViewModel: locationViewModel)
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    HStack {
-                        Text("location_title")
-                            .font(.title)
-                        
-                        Spacer()
-                        
+            ScrollViewReader { proxyReader in
+                ScrollView(.vertical, showsIndicators: false) {
+                    LocationList(locationViewModel: locationViewModel)
+                        .overlay(
+                            GeometryReader { proxy -> Color in
+                                DispatchQueue.main.async {
+                                    if startOffset == 0 {
+                                        self.startOffset = proxy.frame(in: .global).minY
+                                    }
+                                    
+                                    let offset = proxy.frame(in: .global).minY
+                                    self.scrollViewOffset = offset - startOffset
+                                    
+                                    print(self.scrollViewOffset)
+                                }
+                                
+                                return Color.clear
+                            }
+                                .frame(width: 0, height: 0)
+                        )
+                        .id("SCROLL_TO_TOP")
+                }
+                .overlay(
+                    Button(action: {
+                        withAnimation(.spring()) {
+                            proxyReader.scrollTo("SCROLL_TO_TOP", anchor: .top)
+                        }
+                    }, label: {
+                        Image(systemName: "arrow.up")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.theme.red)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.09), radius: 5, x: 5, y: 5)
+                    })
+                    .padding(.trailing)
+                    .padding(.bottom, getSafeAreaInsets().bottom == 0 ? 12 : 0)
+                    .opacity(min(max(-scrollViewOffset / 100, 0), 1))
+                    .animation(.easeInOut, value: scrollViewOffset)
+                    ,alignment: .bottomTrailing
+                )
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            Text("location_title")
+                                .font(.title)
+                            
+                            Spacer()
+                            
+                        }
                     }
                 }
             }
-        }
-        .navigationViewStyle(.stack)
-        .onAppear {
-            locationViewModel.fetchLocations()
+            .navigationViewStyle(.stack)
+            .onAppear {
+                locationViewModel.fetchLocations()
+            }
         }
     }
 }
