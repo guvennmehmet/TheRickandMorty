30246010b2b60357defb34a22a752498fe0c9fa0
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-21 09:49:20 +0300

    Created the AuthenticationView

---
+++ TheRickandMorty/Authentication/AuthenticationView.swift
@@ -0,0 +1,37 @@
+//
+//  AuthenticationView.swift
+//  TheRickandMorty
+//
+//  Created by Mehmet Güven on 21.08.2023.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack {
+            NavigationLink {
+                Text("Hello")
+            } label: {
+                Text("Sign In With Email")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Sign In")
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AuthenticationView()
+        }
+    }
+}
