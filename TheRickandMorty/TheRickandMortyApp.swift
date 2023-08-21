c704cfdec2b4e0a96a36d709ce941520b2cd6df0
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-21 09:12:45 +0300

    Created Firebase connection to Xcode

--- TheRickandMorty/TheRickandMortyApp.swift
+++ TheRickandMorty/TheRickandMortyApp.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TheRickandMortyApp: App {
     @StateObject private var characterViewModel = CharacterViewModel()
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
@@ -20,4 +23,14 @@ struct TheRickandMortyApp: App {
             }
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      print("Configured Firebase!")
+
+    return true
+  }
 }
